b71b7d8e8c3277448d5e6e520d7757af6e242498
Sufyan Afzal <141658175+sufyan-afzal7@users.noreply.github.com>
2023-09-15 11:08:04 +0500

    1.cpp

--- PD LAB 01/1.cpp
+++ PD LAB 01/1.cpp
@@ -1,1 +1,141 @@
+#include <iostream>
+using namespace std;
+main(){
 
+cout<<"                        .                               :::::::::::::::::::::::::::::::::::::::::::::::::::::::                                         "<<endl;
+cout<<"                       . .                              ::                                                   ::                                    "<<endl;
+cout<<"                      .   .                             ::                                                   ::                        "<<endl;
+cout<<"                     .     .                            ::                                                   ::                            "<<endl;
+cout<<"                    .       .                           ::                                                   ::                                                      "<<endl;
+cout<<"                   .         .                          ::                                                   ::                                  "<<endl;
+cout<<"                  .           .                         ::                                                   ::                                    "<<endl;
+cout<<"                 .             .                        ::                                                   ::                  "<<endl;
+cout<<"                .               .                       ::                                                   ::                                     "<<endl;
+cout<<"               .                 .                      ::                                                   ::                      "<<endl;
+cout<<"              .                   .                     ::                                                   ::                 "<<endl;
+cout<<"             .                     .                    ::                                                   ::                        "<<endl;
+cout<<"            .                       .                   ::                                                   ::                  "<<endl;
+cout<<"           .                         .                  ::                                                   ::                 "<<endl;
+cout<<"          .                           .                 ::                                                   ::                   "<<endl;
+cout<<"         .                             .                ::                                                   ::                "<<endl;
+cout<<"        .                               .               ::                                                   ::                "<<endl;
+cout<<"       .                                 .              ::                                                   ::                "<<endl;
+cout<<"      .                                   .             ::                                                   ::                   "<<endl;
+cout<<"     .......................................            :::::::::::::::::::::::::::::::::::::::::::::::::::::::              "<<endl;
+
+
+cout<<"                ::::::::::::::::::::::::                                                                                                                                "<<endl;
+cout<<"              ::                        ::                                                                                                                             "<<endl;
+cout<<"            ::                            ::                                                                                                                            "<<endl;
+cout<<"          ::                                ::                                                                                                                           "<<endl;
+cout<<"        ::                                    ::                                                                                                                      "<<endl;
+cout<<"      ::                                        ::                                                                                                                      "<<endl;
+cout<<"    ::                                            ::                   ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::                     "<<endl;
+cout<<"    ::                                            ::                  ::                                                            ::              "<<endl;
+cout<<"    ::                                            ::                 ::                                                            ::               "<<endl;
+cout<<"    ::                                            ::                ::                                                            ::                 "<<endl;
+cout<<"    ::                                            ::               ::                                                            ::               "<<endl;
+cout<<"    ::                                            ::              ::                                                            ::                "<<endl;
+cout<<"    ::                                            ::             ::                                                            ::                    "<<endl;
+cout<<"    ::                                          ::              ::                                                            ::                    "<<endl;
+cout<<"      ::                                      ::               ::                                                            ::                    "<<endl;
+cout<<"        ::                                  ::                ::                                                            ::                  "<<endl;
+cout<<"          ::                              ::                 ::                                                            ::                     "<<endl;
+cout<<"            ::                          ::                  ::                                                            ::                      "<<endl;
+cout<<"              ::                      ::                   ::                                                            ::                        "<<endl;
+cout<<"                ::::::::::::::::::::::                    ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::                                 "<<endl;
+
+
+
+cout<<"         :::::::::::::::::::::::::::::::::::::::::::::::                                "<<endl;
+cout<<"         ::                                           ::                                 "<<endl;
+cout<<"         ::                                           ::                            "<<endl;
+cout<<"         ::                                           ::                                     "<<endl;
+cout<<"         ::                                           ::                                    "<<endl;
+cout<<"         ::                                           ::                                               "<<endl;
+cout<<"         ::                                           ::                                   "<<endl;
+cout<<"         ::                                           ::                                             "<<endl;
+cout<<"         ::                                           ::                                               "<<endl;
+cout<<"         ::                                           ::                                           "<<endl;
+cout<<"         ::                                           ::                                                 "<<endl;
+cout<<"         ::                                           ::                                                 "<<endl;
+cout<<"         ::                                           ::                                                             "<<endl;
+cout<<"         ::                                           ::                                                  "<<endl;
+cout<<"         ::                                           ::                                   "<<endl;
+cout<<"         :::::::::::::::::::::::::::::::::::::::::::::::                                       "<<endl;
+cout<<"         ::::                                        ::::                                      "<<endl;
+cout<<"         ::  ::                                      ::  ::                                         "<<endl;
+cout<<"         ::    ::                                    ::    ::                                          "<<endl;
+cout<<"         ::      ::                                  ::      ::                                         "<<endl;
+cout<<"         ::        ::                                ::        ::                                              "<<endl;
+cout<<"         ::          ::                              ::          ::                                       "<<endl;
+cout<<"         ::            ::                            ::            ::                                           "<<endl;
+cout<<"         ::              ::                          ::              ::                                                  "<<endl;
+cout<<"         ::                ::                                          ::                                                   "<<endl;
+cout<<"         ::                  ::                                          ::                                             "<<endl;
+cout<<"         ::                    ::                                          ::                                        "<<endl;
+cout<<"         ::                      ::::::::::::::::::::::::::::::::::::::::::::                                     "<<endl;
+cout<<"         ::                      ::                                        ::                                     "<<endl;
+cout<<"                                 ::                                        ::                                           "<<endl;
+cout<<"                                 ::                                        ::                                      "<<endl;
+cout<<"                                 ::                                        ::                                              "<<endl;
+cout<<"                                 ::                                        ::                                          "<<endl;
+cout<<"                                 ::                                        ::                                                "<<endl;
+cout<<"                                 ::                                        ::                                                "<<endl;
+cout<<"                                 ::                                        ::                                                       "<<endl;
+cout<<"                                 ::                                        ::                                                          "<<endl;
+cout<<"                                 ::                                        ::                                                                                 "<<endl;
+cout<<"                                 ::                                        ::                                                                                "<<endl;
+cout<<"                                 ::                                        ::                                                                               "<<endl;
+cout<<"                                                                                                                                                          "<<endl;
+cout<<"                                                                                                                                                                  "<<endl;
+cout<<"                                                                                                                                                                     "<<endl;
+cout<<"                                                                                                                                                                  "<<endl;
+cout<<"                                                                                                                                                                    "<<endl;
+cout<<"                                                                                                                                                          "<<endl;
+
+
+cout<<"                           :::::::::::::::::::::::::::::::::::::::::::::::::::::::                                                  "<<endl;
+cout<<"                         ::                                                      ::                 "<<endl;
+cout<<"                        ::                                                         ::               	"<<endl;
+cout<<"                       ::                                                           ::  			 "<<endl;
+cout<<"                       ::                                                            ::  		  "<<endl;
+cout<<"                       ::                                                            ::  		 "<<endl;
+cout<<"                       ::                                                            ::  		   "<<endl;
+cout<<"                       ::                                                            ::   		    "<<endl;
+cout<<"                       ::                                                            ::        "<<endl;
+cout<<"                       ::                                                            ::  		"<<endl;
+cout<<"                       ::                                                            ::           "<<endl;
+cout<<"                       ::                                                            ::   	    "<<endl;
+cout<<"                       ::                                                            ::            "<<endl;
+cout<<"                       ::                                                            ::    	  "<<endl;
+cout<<"                       ::                                                            ::    	       "<<endl;
+cout<<"                       ::                                                            ::     	    "<<endl;
+cout<<"        ::::::::::     ::                                                            ::      	"<<endl;
+cout<<"       ::        ::    ::                                                            ::      	  "<<endl;
+cout<<"       ::         ::   ::                                                            ::		 "<<endl;
+cout<<"        ::         ::  ::                                                            :: 			 "<<endl;
+cout<<"         ::         :: ::                                                            :: 		"<<endl;
+cout<<"          ::         ::::                                                            :: 			"<<endl;
+cout<<"           ::         :::                                                            :: 			"<<endl;
+cout<<"            ::                                                                       ::  		 "<<endl;
+cout<<"             ::                                                                      ::    		  "<<endl;
+cout<<"              ::                                                                     ::     		"<<endl;
+cout<<"               ::                                                                    ::      		 "<<endl;
+cout<<"                ::                                                                  ::       		   "<<endl;
+cout<<"                 ::                                                                ::            		"<<endl;
+cout<<"                  ::                                                              ::         		        "<<endl;
+cout<<"                   ::                                                            ::              		   "<<endl;
+cout<<"                    ::                                                          ::              		"<<endl;
+cout<<"                     ::                                                        ::                    "<<endl;
+cout<<"                      ::                                                      ::            		   "<<endl;
+cout<<"                       ::                                                    ::               		      "<<endl;
+cout<<"                        ::::::::::::::::::::::::::::::::::::::::::::::::::::::                                                                   "<<endl;
+cout<<"                                :::                                       :::                          "<<endl;
+cout<<"                                :::                                       :::                             "<<endl;
+cout<<"                                 :::                                     :::                       "<<endl;
+cout<<"                                 :::::::::::::::::::::::::::::::::::::::::::                                                         "<<endl;
+
+
+            
+}
