334c34c5947be52cdeb5ddc3fc7619e89292730e
Sufyan Afzal <141658175+sufyan-afzal7@users.noreply.github.com>
2023-09-16 16:35:57 +0500

    Update Task03(CP).cpp

--- PD Lab 02/Task03(CP).cpp
+++ PD Lab 02/Task03(CP).cpp
@@ -2,11 +2,11 @@
 using namespace std;
 main(){
 
-	cout << "      /\\    " << endl;
-	cout << "   __/~~\\__     " << endl;
-	cout << " /   |  |    \\" << endl;
-	cout << "=====.  .======         " << endl;
-	cout << "     ||||    " << endl;
+	cout << "      /\\   			 " << endl;
+	cout << "   __/~~\\__    		 " << endl;
+	cout << " /   |  |    \\		 " << endl;
+	cout << "=====.  .======        	 " << endl;
+	cout << "     ||||    			 " << endl;
    
 
 }
