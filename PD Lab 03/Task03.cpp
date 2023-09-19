f0873fd8fe346cfd1a3d98bfb132c1269f271f82
Sufyan Afzal <141658175+sufyan-afzal7@users.noreply.github.com>
2023-09-19 19:56:12 +0500

    Create Task03.cpp

---
+++ PD Lab 03/Task03.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+main()
+{
+
+  float length;
+  float width;
+  float area;
+
+  cout << "Enter the length of the rectangle: ";
+  cin >> length;
+  cout << "Enter the Width of the rectangle: ";
+  cin >> width;
+	area = length * width;
+  cout << "The area of the rectangle is: "<< area;
+
+
+}
